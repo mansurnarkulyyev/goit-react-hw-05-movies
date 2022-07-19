6898ca19345c2e1e6617ac830acd8459e8f8aad8
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-07-19 20:38:51 +0300

    .

---
+++ build/static/js/93.e578e0c1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_router_dom_example=self.webpackChunkreact_router_dom_example||[]).push([[93],{3093:function(e,n,r){r.r(n),r.d(n,{default:function(){return c}});var t=r(501),a=r(184),c=function(){return(0,a.jsx)("main",{children:(0,a.jsxs)("div",{className:"container",children:[(0,a.jsx)("h2",{children:"Page not found"}),(0,a.jsx)(t.rU,{to:"/",children:"To home page"})]})})}}}]);
+//# sourceMappingURL=93.e578e0c1.chunk.js.map
